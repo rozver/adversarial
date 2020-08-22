2580383daaf93ddc40f21e9e27dd1003a31b9c3f
RoZvEr <httodoroff@gmail.com>
2020-08-23 00:42:15 +0300

    Implement a targeted transferable attack

--- transferable_attack.py
+++ transferable_attack.py
@@ -5,6 +5,7 @@ from adversarial_transfer_models import get_models_dict
 from untargeted_imagenet import save_images
 import datetime
 import os
+from math import sqrt
 
 
 def custom_transfer_loss(model, x, target):
@@ -16,8 +17,33 @@ def custom_transfer_loss(model, x, target):
         current_model = models_dict[key].cuda()
         prediction = current_model(x.cuda())
 
-        current_loss = -criterion(prediction.cuda(), target.cuda())
-        loss = loss + current_loss
+        original_class = target.item()
+        adv_class = torch.argmax(prediction[0]).item()
+
+        print(original_class)
+        print(adv_class)
+
+        weight = sqrt((adv_class-original_class)**2)+0.01
+
+        loss = loss - weight*criterion(prediction.cuda(), target.cuda())
+
+    loss = loss/(len(models_dict.keys()))
+
+    return loss, None
+
+
+def custom_transfer_loss_targeted(model, x, target):
+    criterion = torch.nn.CrossEntropyLoss()
+    models_dict = get_models_dict()
+    loss = torch.zeros([1]).cuda()
+
+    for key in models_dict.keys():
+        current_model = models_dict[key].cuda()
+        prediction = current_model(x.cuda())
+
+        print(torch.argmax(prediction[0]).item())
+
+        loss = loss + criterion(prediction.cuda(), target.cuda())
 
     loss = loss/(len(models_dict.keys()))
 
@@ -25,17 +51,17 @@ def custom_transfer_loss(model, x, target):
 
 
 def main():
-    IMAGES_LOCATION = 'results/images/'
-    CURRENT_TIME = str(datetime.datetime.now().strftime('%d-%m-%Y_%H:%M:%S'))
-    SAVE_LOCATION = IMAGES_LOCATION + CURRENT_TIME
-    os.mkdir(SAVE_LOCATION)
+    images_location = 'results/images/'
+    current_time = str(datetime.datetime.now().strftime('%d-%m-%Y_%H:%M:%S'))
+    save_location = images_location + current_time
+    os.mkdir(save_location)
 
     kwargs = {
         'custom_loss': custom_transfer_loss,
         'constraint': 'inf',
         'eps': 16.0 / 255.0,
         'step_size': 1.0 / 255.0,
-        'iterations': 40,
+        'iterations': 50,
         'do_tqdm': True,
         'targeted': True,
     }
@@ -46,7 +72,7 @@ def main():
                                       pytorch_pretrained=True)
     model = model.cuda()
 
-    dataset = torch.load('./dataset/imagenet-dogs-images.pt')
+    dataset = torch.load('./dataset/imagenet-dogs-images-grayscale.pt')
     image_loader = torch.utils.data.DataLoader(dataset, batch_size=4, num_workers=2)
     labels = torch.load('./dataset/imagenet-dogs-labels.pt')
 
@@ -58,18 +84,18 @@ def main():
 
         _, images_adversarial = model(images_batch.cuda(), targ.cuda(), make_adv=True, **kwargs)
 
-        test_model = model.eval()
+        eval_model = model.eval()
 
-        original_pred = test_model(images_batch.cuda())
+        original_pred = eval_model(images_batch.cuda())
         original_pred = torch.argmax(original_pred[0]).item()
-        adversarial_pred = test_model(images_adversarial.cuda())
+        adversarial_pred = eval_model(images_adversarial.cuda())
         adversarial_pred = torch.argmax(adversarial_pred[0]).item()
 
         print('Original prediction: ' + str(original_pred))
         print('Adversarial prediction: ' + str(adversarial_pred))
         print('')
 
-        save_images(images_batch, images_adversarial, batch_index, SAVE_LOCATION)
+        save_images(images_batch, images_adversarial, batch_index, save_location)
 
     print('Finished!')
 
