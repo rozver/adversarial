f12e10d8862d95b1bd7bed0feb36a21180ff543f
RoZvEr <httodoroff@gmail.com>
2020-09-02 20:05:44 +0300

    Fix the attack_image.py script

--- attack_image.py
+++ attack_image.py
@@ -2,6 +2,7 @@ import os
 import torch
 from PIL import Image
 import argparse
+import torchvision
 from torchvision import transforms
 from robustness.datasets import ImageNet
 from robustness.model_utils import make_and_restore_model
@@ -23,43 +24,47 @@ def main():
         transform = transforms.Compose([
             transforms.ToTensor(),
         ])
+
         image = transform(image)
         image = image.unsqueeze(0).cuda()
 
         # Attack parameters
         kwargs = {
                 'constraint': 'inf',
-                'eps': 16.0/255,
-                'step_size': 1/255.0,
+                'eps': 16.0/255.0,
+                'step_size': 1.0/255.0,
                 'iterations': 500,
                 'do_tqdm': True,
-                'targeted': False,
             }
 
         # Set the dataset for the robustness model
-        dataset = ImageNet('dataset/imagenet-airplanes')
+        dataset = ImageNet('dataset/')
 
         # Make a robustness model
         model, _ = make_and_restore_model(arch='resnet50', dataset=dataset,
                                           pytorch_pretrained=True)
-        model = model.eval().cuda()
+        model = model.cuda()
+        eval_model = torchvision.models.resnet50(pretrained=True).cpu().eval()
 
         # Get the model prediction for the original image
-        label = model(image)
+        label = eval_model(image.cpu())
         label = torch.argmax(label[0])
         label = label.view(1).cuda()
 
         # Create an adversarial example of the original images
         _, adversarial_example = model(image, label, make_adv=True, **kwargs)
-        adversarial_prediction, _ = model(adversarial_example)
+
+        # Get the prediction of the model for the adversarial image
+        adversarial_prediction = eval_model(adversarial_example.cpu())
         adversarial_prediction = torch.argmax(adversarial_prediction[0])
 
         # Print the original and the adversarial prediction
         print('Original prediction: ' + str(label.item()))
         print('Adversarial prediction: ' + str(adversarial_prediction.item()))
 
-        # Save the adversarial example in the same folder as the original iamge
-        adversarial_location = args.image[0:-5] + '-adversarial' + args.image[-5:]
+        # Save the adversarial example in the same folder as the original image
+        filename_and_extension = args.image.split('.')
+        adversarial_location = filename_and_extension[0] + '_adversarial.' + filename_and_extension[-1]
         save_image(adversarial_example[0], adversarial_location)
 
     else:
