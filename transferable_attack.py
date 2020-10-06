8ce73631435b9963631479b3334ccab2cecd72aa
RoZvEr <httodoroff@gmail.com>
2020-10-06 14:31:48 +0300

    Implement transferable attack with surrogate models ready to be deployed to the cloud

--- transferable_attack.py
+++ transferable_attack.py
@@ -1,10 +1,9 @@
 import torch
+import torchvision
 from robustness.datasets import ImageNet
 from robustness.model_utils import make_and_restore_model
 from adversarial_transfer_models import get_models_dict
-from untargeted_imagenet import save_images
 import datetime
-import os
 from math import sqrt
 
 
@@ -17,9 +16,8 @@ def custom_transfer_loss(model, x, target):
         current_model = models_dict[key].cuda()
         prediction = current_model(x.cuda())
 
-        original_class = target.item()
+        original_class = torch.argmax(target[0]).item()
         adv_class = torch.argmax(prediction[0]).item()
-
         print(original_class)
         print(adv_class)
 
@@ -51,17 +49,16 @@ def custom_transfer_loss_targeted(model, x, target):
 
 
 def main():
-    images_location = 'results/images/'
+    images_location = 'results/transferable-'
     current_time = str(datetime.datetime.now().strftime('%d-%m-%Y_%H:%M:%S'))
     save_location = images_location + current_time
-    os.mkdir(save_location)
 
     kwargs = {
         'custom_loss': custom_transfer_loss,
         'constraint': 'inf',
-        'eps': 16.0 / 255.0,
+        'eps': 8.0 / 255.0,
         'step_size': 1.0 / 255.0,
-        'iterations': 50,
+        'iterations': 1,
         'do_tqdm': True,
         'targeted': True,
     }
@@ -71,33 +68,19 @@ def main():
     model, _ = make_and_restore_model(arch='resnet50', dataset=dataset,
                                       pytorch_pretrained=True)
     model = model.cuda()
+    eval_model = torchvision.models.resnet50(pretrained=True).cuda().eval()
 
-    dataset = torch.load('./dataset/imagenet-dogs-images-grayscale.pt')
+    dataset = torch.load('./dataset/imagenet-dogs-images.pt')
     image_loader = torch.utils.data.DataLoader(dataset, batch_size=4, num_workers=2)
-    labels = torch.load('./dataset/imagenet-dogs-labels.pt')
+    results = []
 
-    for batch_index, (images_batch, labels_batch) in enumerate(zip(image_loader, labels)):
-        images_batch = images_batch[:1]
-        labels_batch = labels_batch[:1]
-
-        targ = torch.LongTensor([labels_batch])
+    for batch_index, images_batch in enumerate(image_loader):
+        targ = eval_model(images_batch.cuda())
 
         _, images_adversarial = model(images_batch.cuda(), targ.cuda(), make_adv=True, **kwargs)
+        results.append(images_adversarial.cpu())
 
-        eval_model = model.eval()
-
-        original_pred = eval_model(images_batch.cuda())
-        original_pred = torch.argmax(original_pred[0]).item()
-        adversarial_pred = eval_model(images_adversarial.cuda())
-        adversarial_pred = torch.argmax(adversarial_pred[0]).item()
-
-        print('Original prediction: ' + str(original_pred))
-        print('Adversarial prediction: ' + str(adversarial_pred))
-        print('')
-
-        save_images(images_batch, images_adversarial, batch_index, save_location)
-
-    print('Finished!')
+    torch.save({'results': results, 'args': kwargs}, save_location)
 
 
 if __name__ == '__main__':
