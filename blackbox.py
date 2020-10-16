e421890ade77b31fbecb2f7f60159174f19fd229
RoZvEr <httodoroff@gmail.com>
2020-10-16 16:28:12 +0300

    SimBA with dynamic eps

--- blackbox.py
+++ blackbox.py
@@ -43,13 +43,13 @@ def simba_pixels(model, x, y, args):
         p_prim_left = get_probabilities(model, (x + delta + eps * q).clamp(0, 1), y)
 
         if p_prim_left < p:
-            delta = delta + eps * q
+            delta = delta + eps * (p-p_prim_left) * q
             p = p_prim_left
 
         else:
             p_prim_right = get_probabilities(model, (x + delta - eps * q).clamp(0, 1), y)
             if p_prim_right < p:
-                delta = delta - eps * q
+                delta = delta + eps * (p-p_prim_right) * q
                 p = p_prim_left
 
         q[c, w, h] = 0
@@ -84,8 +84,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default='resnet50')
     parser.add_argument('--dataset', type=str, default='dataset/imagenet-airplanes-images.pt')
-    parser.add_argument('--eps', type=float, default=8)
-    parser.add_argument('--num_iterations', type=int, default=10000)
+    parser.add_argument('--eps', type=float, default=10)
+    parser.add_argument('--num_iterations', type=int, default=1)
     parser.add_argument('--save_file_name', type=str, default='results/blackbox-' + time + '.pt')
     args = parser.parse_args()
 
@@ -105,7 +105,7 @@ def main():
         # adversarial_example_nes = fgsm_grad(image.cuda(), grad, args.eps/255.0)
 
         delta = simba_pixels(model, image.cuda(), label.cuda(), args)
-        adversarial_example_simba = image.cuda() + delta
+        adversarial_example_simba = (image.cuda() + delta).clamp(0, 1)
 
         # adversarial_predictions_nes = torch.argmax(model(adversarial_example_nes.unsqueeze(0)))
         adversarial_predictions_simba = torch.argmax(model(adversarial_example_simba.unsqueeze(0)))
