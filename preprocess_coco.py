cd7c97ee8fc684a7ee9f2c43b09e022615e7a540
RoZvEr <httodoroff@gmail.com>
2020-11-04 02:39:52 +0200

    Add script for COCO dataset preprocessing

---
+++ preprocess_coco.py
@@ -0,0 +1,60 @@
+import torch
+from torchvision import transforms
+from pycocotools.coco import COCO
+from PIL import Image
+import argparse
+import os
+
+
+def get_coco_images_and_masks(location, category):
+    transform = transforms.ToTensor()
+
+    images_and_masks = []
+    annotations_file_location = os.path.join(location, 'annotations/instances_val2017.json')
+    images_folder_location = os.path.join(location, 'images/val2017')
+    coco = COCO(annotations_file_location)
+
+    cat_ids = coco.getCatIds(catNms=category)
+    img_ids = coco.getImgIds(catIds=cat_ids)
+    images = coco.loadImgs(img_ids)
+
+    for image in images:
+        annotations_ids = coco.getAnnIds(imgIds=image['id'], catIds=cat_ids, iscrowd=None)
+        annotations = coco.loadAnns(annotations_ids)
+
+        image_pil = Image.open(os.path.join(images_folder_location, image['file_name']))
+        image_tensor = transform(image_pil)
+
+        mask = coco.annToMask(annotations[0])
+
+        for i in range(len(annotations)):
+            mask += coco.annToMask(annotations[i])
+
+        mask = torch.from_numpy(mask)
+        images_and_masks.append([image_tensor, mask])
+
+    return images_and_masks
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--location', type=str, required=True)
+    parser.add_argument('--category', type=str, required=True)
+    args = parser.parse_args()
+
+    if os.path.exists(args.location):
+        categories_file = open(os.path.join(args.location, 'categories_list.txt'))
+        file_read = categories_file.read()
+        if '\'{}\''.format(args.category) in file_read:
+            categories_file.close()
+            images_and_masks = get_coco_images_and_masks(location=args.location, category=args.category)
+        else:
+            raise ValueError('Incorrect COCO category specified!')
+    else:
+        raise ValueError('Incorrect dataset location specified!')
+
+    torch.save(images_and_masks, os.path.join(args.location, args.category + '.pt'))
+
+
+if __name__ == '__main__':
+    main()
