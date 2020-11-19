842faac1125c641e753ab8cb87dff03021a1511c
RoZvEr <httodoroff@gmail.com>
2020-11-20 01:44:00 +0200

    Add results evaluation support for COCO dataset with masks and fix implementation error when calculating attack success rate

--- evaluate_results.py
+++ evaluate_results.py
@@ -1,8 +1,8 @@
 import torch
-from matplotlib import pyplot as plt
 import os
 from torchvision.utils import save_image
 import argparse
+from matplotlib import pyplot as plt
 
 
 def plot_adversarial_examples(results):
@@ -11,41 +11,41 @@ def plot_adversarial_examples(results):
         plt.show()
 
 
-def save_images_and_noises(results, results_location, dataset):
+def save_images(results, results_location, dataset):
     results_images_folder = os.path.dirname(results_location)+'/images/'+results_location.split('/')[-1][:-3]
+    original_directory = results_images_folder + '/original/'
+    adversarial_directory = results_images_folder + '/adversarial/'
+    noises_directory = results_images_folder + '/noises/'
+    masks_directory = results_images_folder + '/masks/'
 
     if not os.path.exists(results_images_folder):
-        os.makedirs(results_images_folder)
+        os.makedirs(original_directory)
+        os.makedirs(adversarial_directory)
+        os.makedirs(noises_directory)
 
-    for (image_index, adversarial_example) in enumerate(results['adversarial_examples']):
-        original_image = dataset[image_index]
+        if results['args'].masks:
+            os.makedirs(masks_directory)
+
+    for index, (original_image, adversarial_example) in enumerate(zip(dataset, results['adversarial_examples'],)):
+        if results['args'].masks:
+            original_image, mask = original_image
+            save_image(mask,  (masks_directory + str(index) + '.png'), normalize=True)
+
         noise = original_image - adversarial_example
 
-        images_save_path = results_images_folder + '/' + str(image_index)
-        save_image(original_image,
-                   images_save_path + '_original.png', normalize=True)
-        save_image(adversarial_example,
-                   images_save_path + '_adversarial.png', normalize=True)
-        save_image(noise,
-                   images_save_path + '_noise.png', normalize=True)
+        save_image(original_image, (original_directory + str(index) + '.png'), normalize=True)
+        save_image(adversarial_example, (adversarial_directory + str(index) + '.png'), normalize=True)
+        save_image(noise, (noises_directory + str(index) + '.png'), normalize=True)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--location', type=str, required=True)
-    parser.add_argument('--dataset', type=str, default='dataset/imagenet-airplanes-images.pt')
     parser.add_argument('--save_images', default=False, action='store_true')
     args = parser.parse_args()
 
-    successful_attacks = 0
-    all_attacks = 0
     setups_and_results = []
-    dataset = None
-
     files = os.listdir(args.location)
-
-    if args.save_images:
-        dataset = torch.load(args.dataset)
 
     for file in files:
         if not file.endswith('.pt'):
@@ -53,6 +53,7 @@ def main():
 
         results_location = os.path.join(args.location, file)
         results = torch.load(results_location)
+        successful_attacks = 0
 
         for predictions in results['predictions']:
             original_class = torch.argmax(predictions['original']).item()
@@ -61,14 +62,14 @@ def main():
             if original_class != adversarial_class:
                 successful_attacks += 1
 
-            all_attacks += 1
-
+        success_rate = round(successful_attacks/len(results['predictions']), 2)
         setups_and_results.append(str(results['args']) + '\nAttack success rate: ' +
-                                  str(successful_attacks/all_attacks*100) +
+                                  str(success_rate) +
                                   '\n')
 
         if args.save_images:
-            save_images_and_noises(results, results_location, dataset)
+            dataset = torch.load(results['args'].dataset)
+            save_images(results, results_location, dataset)
 
     with open(os.path.join(args.location, 'setups_and_results.txt'), 'w') as file:
         for result in setups_and_results:
