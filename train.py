a82e253c397f1c018b6c3f162db625ab25a6457e
RoZvEr <httodoroff@gmail.com>
2020-11-29 22:32:56 +0200

    Implement model training (both normal and adversarial)

--- train.py
+++ train.py
@@ -3,46 +3,101 @@ from pgd import Attacker, MODELS_DICT
 import argparse
 
 
+class Trainer:
+    def __init__(self, model, training_args_dict, pgd_args_dict,
+                 criterion=torch.nn.CrossEntropyLoss(),
+                 optimizer=torch.optim.SGD):
+        self.model = model
+        self.training_args_dict = training_args_dict
+        self.pgd_args_dict = pgd_args_dict
+        self.adversarial = training_args_dict['adversarial']
+        self.attacker = None
+        self.criterion = criterion
+        self.optimizer = optimizer(self.model.parameters(), lr=1e-1)
+
+    def fit(self, data_loader, epochs):
+        self.model = self.model.cuda().train()
+        for epoch in range(epochs):
+            for images_batch, labels_batch in data_loader:
+                if self.adversarial:
+                    images_batch = self.get_adversarial_examples(images_batch, labels_batch)
+
+                self.optimizer.zero_grad()
+
+                predictions = self.model.cuda()(images_batch.cuda())
+                loss = self.criterion(predictions, labels_batch.cuda())
+                print(loss)
+
+                loss.backward()
+                self.optimizer.step()
+
+    def switch_to_normal(self):
+        self.adversarial = False
+
+    def switch_to_adversarial(self):
+        self.adversarial = True
+
+    def get_model(self):
+        return self.model
+
+    def get_adversarial_examples(self, images_batch, labels_batch):
+        mask = None
+        if self.attacker is None:
+            self.attacker = Attacker(self.model.cpu().eval(), self.pgd_args_dict)
+
+        adversarial_batch = None
+
+        for image, label in zip(images_batch, labels_batch):
+            if mask is None:
+                mask = torch.ones(image.size())
+
+            if adversarial_batch is None:
+                adversarial_batch = (self.attacker(image=image, mask=mask, target=label)).unsqueeze(0)
+                continue
+
+            adversarial_example = self.attacker(image=image, mask=mask, target=label)
+            adversarial_batch = torch.cat((adversarial_batch, adversarial_example.unsqueeze(0)), 0)
+
+        return adversarial_batch
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, choices=MODELS_DICT.keys(), default='resnet50')
     parser.add_argument('--dataset', type=str, default='dataset/imagenet-airplanes-images.pt')
-    parser.add_argument('--masks', default=False, action='store_true')
-    parser.add_argument('--eps', type=float, default=8)
-    parser.add_argument('--norm', type=str, choices=['l2', 'linf'], default='linf')
-    parser.add_argument('--step_size', type=float, default=1)
-    parser.add_argument('--num_iterations', type=int, default=10)
     parser.add_argument('--epochs', type=int, default=10)
-    parser.add_argument('--targeted', default=False, action='store_true')
-    parser.add_argument('--eot', default=False, action='store_true')
-    parser.add_argument('--transfer', default=False, action='store_true')
-    args = parser.parse_args()
+    parser.add_argument('--adversarial', default=False, action='store_true')
+    args_ns = parser.parse_args()
+
+    args_dict = vars(args_ns)
+    pgd_args_dict = {
+        'model': 'resnet50',
+        'dataset': 'dataset/imagenet-airplanes-images.pt',
+        'masks': False,
+        'eps': 8/255.0,
+        'norm': 'linf',
+        'step_size': 1/255.0,
+        'num_iterations': 10,
+        'targeted': False,
+        'eot': False,
+        'transfer': False,
+    }
 
     images = torch.load('dataset/imagenet-airplanes-images.pt')
     labels = torch.load('dataset/imagenet-airplanes-labels.pt')
 
     images_loader = torch.utils.data.DataLoader(images, batch_size=10, num_workers=4)
     labels_loader = torch.utils.data.DataLoader(labels, batch_size=10, num_workers=4)
+    data_loader = zip(images_loader, labels_loader)
 
-    model = MODELS_DICT.get(args.model).train()
-    criterion = torch.nn.CrossEntropyLoss()
-    opt = torch.optim.SGD(model.parameters(), lr=1e-1)
+    model = MODELS_DICT.get(args_dict['model']).cuda().train()
 
-    attacker = Attacker(model, args)
-    for epoch in range(args.epochs):
-        for images_batch, labels_batch in zip(images_loader, labels_loader):
-            for image, label in zip(images_batch, labels_batch):
-                delta = attacker(image, torch.ones(image.size()), label) - image
-                adversarial_prediction = model.cuda()((image+delta).cuda().unsqueeze(0))
+    trainer = Trainer(model, args_dict, pgd_args_dict)
+    trainer.switch_to_adversarial()
+    trainer.fit(data_loader, 1)
 
-                loss = criterion(adversarial_prediction, torch.LongTensor([label]).cuda())
-                print(loss)
-
-                opt.zero_grad()
-                loss.backward()
-                opt.step()
-
-    torch.save(model.state_dict(), 'models/' + args.model + '_robust_2.pt')
+    torch.save({'state_dict': model.state_dict(), 'training_args': args_dict, 'pgd_args': pgd_args_dict},
+               'models/' + args_dict['model'] + '_robust.pt')
 
 
 if __name__ == '__main__':
