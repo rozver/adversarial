bc31488313145ada6b19ed033f8c1a460d292b28
RoZvEr <httodoroff@gmail.com>
2020-12-20 02:44:11 +0200

    Use np.flip() to reverse array in gradient_analysis.py

--- gradient_analysis.py
+++ gradient_analysis.py
@@ -29,7 +29,7 @@ def get_sorted_order(grad, size):
     if not 0 < size < grad.size(0):
         raise ValueError('Invalid size entered!')
 
-    order = np.argsort(grad.cpu())[::-1][:size]
+    order = np.flip(np.argsort(grad.cpu()))[:size]
     return order
 
 
