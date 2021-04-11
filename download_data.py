7b76350a554c285b76e2574d4899dd92e62c8272
RoZvEr <httodoroff@gmail.com>
2021-04-12 01:19:02 +0300

    Replace gdown with requests in download_data.py

--- download_data.py
+++ download_data.py
@@ -1,15 +1,24 @@
-import gdown
+import requests
 import zipfile
 import os
 
 # Download zip file from Google Drive
 drive_url = 'https://drive.google.com/uc?id=13vhvMGc56CdQx7DrGtVVi86GFBm5Zu2J'
-output_file = 'adversarial_data.zip'
-gdown.download(drive_url, output_file, quiet=False)
+output_location = 'adversarial_data.zip'
+
+print('Downloading data...')
+r = requests.get(drive_url, allow_redirects=True)
+with open(output_location, 'wb') as output_file:
+    output_file.write(r.content)
+print('Finished!')
 
 # Extract the zip file into the current directory
-with zipfile.ZipFile(output_file, 'r') as file:
-    file.extractall('')
+print('Unzipping file...')
+with zipfile.ZipFile(output_location, 'r') as zip_file:
+    zip_file.extractall('')
+print('Finished!')
 
 # Remove the zip file
-os.remove(output_file)
+print('Removing the zip file...')
+os.remove(output_location)
+print('Finished!')
